38ae7749661468a116c089ca6a2ca87c06325e36
PIYUSH RAJ <piyush.raj.bhs@gmail.com>
2017-05-04 21:29:11 +0530

    Add files via upload

---
+++ App_Code/ConfigurationFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ConfigurationFile
+/// </summary>
+public class ConfigurationFile
+{
+    public SqlConnection con = new SqlConnection();
+    public ConfigurationFile()
+    {
+        con.ConnectionString = ConfigurationManager.ConnectionStrings["SQLConnectionString"].ConnectionString;
+    }
+
+    public float AutoGenerate()
+    {
+        Random rn = new Random();
+        float autogen = rn.Next(11111, 99999);
+        return autogen;
+    }
+}
